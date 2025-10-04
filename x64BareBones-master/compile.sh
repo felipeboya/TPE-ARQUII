e114936acfafa29933dc69433b2dffc801891dc5
felipeboya <fboyaciyan@itba.edu.ar>
2025-10-04 12:15:14 -0300

    script compile

---
+++ x64BareBones-master/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker start tpe-arqui 
+docker exec -it tpe-arqui make clean -C /root/Toolchain
+docker exec -it tpe-arqui make clean -C /root/
+docker exec -it tpe-arqui make -C /root/Toolchain
+docker exec -it tpe-arqui make -C /root/
+docker stop tpe-arqui 
