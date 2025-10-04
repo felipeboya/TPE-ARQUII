6d09fcab044cbaa37a28a275df1a08827eb0dbb4
felipeboya <fboyaciyan@itba.edu.ar>
2025-10-04 12:23:56 -0300

    Update compile.sh

--- x64BareBones-master/compile.sh
+++ x64BareBones-master/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker start tpe-arqui 
-docker exec -it tpe-arqui make clean -C /root/Toolchain
-docker exec -it tpe-arqui make clean -C /root/
-docker exec -it tpe-arqui make -C /root/Toolchain
-docker exec -it tpe-arqui make -C /root/
-docker stop tpe-arqui 
+docker start TPE-ARQUI 
+docker exec -it TPE-ARQUI make clean -C /root/Toolchain
+docker exec -it TPE-ARQUI make clean -C /root/
+docker exec -it TPE-ARQUI make -C /root/Toolchain
+docker exec -it TPE-ARQUI make -C /root/
+docker stop TPE-ARQUI 
